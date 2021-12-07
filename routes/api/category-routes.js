9f29f39257bd2342084053d306992e0f4a301278
MckinleyVJ <87624839+mckinleyvj@users.noreply.github.com>
2021-12-07 20:43:21 +1100

    fix: removed comments

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -46,7 +46,6 @@ router.post('/', (req, res) => {
 });
 
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
   try {
     Category.update(req.body, {
       where: {
@@ -66,7 +65,6 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
   try {
     Category.destroy({
       where: {
